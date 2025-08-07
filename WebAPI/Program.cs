c91064b7812296c37f4c56f6c0be6d238a96f400
Minhaza <minhazaaa1006@gmail.com>
2025-08-07 14:12:59 +0700

    Init API structure

---
+++ WebAPI/Program.cs
@@ -0,0 +1,61 @@
+
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddHttpContextAccessor();
+
+            builder.Services.AddAuthentication("Bearer")
+           .AddJwtBearer("Bearer", options =>
+           {
+               var jwt = builder.Configuration.GetSection("Jwt");
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateIssuerSigningKey = true,
+                   ValidIssuer = jwt["Issuer"],
+                   ValidAudience = jwt["Audience"],
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]))
+               };
+           });
+
+
+            // Register AutoMapper
+            builder.Services.AddAutoMapper(typeof(Program));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
