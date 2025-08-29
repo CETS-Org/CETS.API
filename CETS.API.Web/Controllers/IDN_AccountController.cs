301a9a9c9e0798dcf513bbc2f44cab6f8becfc96
Minhaza <minhazaaa1006@gmail.com>
2025-08-29 21:14:37 +0700

    feat: create account controller

--- CETS.API.Web/Controllers/IDN_AccountController.cs
+++ CETS.API.Web/Controllers/IDN_AccountController.cs
@@ -20,8 +20,37 @@ namespace CETS.API.Web.Controllers
         [HttpGet("statuses")]
         public async Task<IActionResult> GetAccountStatusesAsync()
         {
-            var statuses = await _accountService.GetAccountStatusesAsync();
+            var statuses = await _accountService.GetStatusesAsync();
             return Ok(statuses);
+        }
+
+        [HttpGet("accounts")]
+        public async Task<IActionResult> GetAllAccountsAsync()
+        {
+            var accounts = await _accountService.GetAllAsync();
+            return Ok(accounts);
+        }
+
+        [HttpGet("accounts/{id:guid}")]
+        public async Task<IActionResult> GetAccountByIdAsync(Guid id)
+        {
+            var account = await _accountService.GetByIdAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return Ok(account);
+        }
+
+        [HttpGet("accounts/email")]
+        public async Task<IActionResult> GetAccountByEmailAsync([FromQuery] string email)
+        {
+            var account = await _accountService.GetByEmailAsync(email);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return Ok(account);
         }
     }
 }
