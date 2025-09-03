aaef349c19b0933b2d6113691b069fae46ec62d1
Minhaza <minhazaaa1006@gmail.com>
2025-09-03 15:00:58 +0700

    feat: add COM apis

---
+++ CETS.API.Web/Controllers/COM/COM_FeedbackController.cs
@@ -0,0 +1,57 @@
+using Application.Interfaces.COM;
+using DTOs.COM_Feedback.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CETS.API.Web.Controllers.COM
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class COM_FeedbackController : ControllerBase
+	{
+		private readonly ICOM_FeedbackService _service;
+
+		public COM_FeedbackController(ICOM_FeedbackService service)
+		{
+			_service = service;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllAsync()
+		{
+			var items = await _service.GetAllAsync();
+			return Ok(items);
+		}
+
+		[HttpGet("{id:guid}")]
+		public async Task<IActionResult> GetByIdAsync(Guid id)
+		{
+			var item = await _service.GetByIdAsync(id);
+			if (item == null) return NotFound();
+			return Ok(item);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateAsync([FromBody] CreateFeedbackRequest request)
+		{
+			var created = await _service.CreateAsync(request);
+			return Created("Created", created);
+		}
+
+		[HttpPut("{id:guid}")]
+		public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] UpdateFeedbackRequest request)
+		{
+			var updated = await _service.UpdateAsync(id, request);
+			return Ok(updated);
+		}
+
+		[HttpDelete("{id:guid}")]
+		public async Task<IActionResult> SoftDeleteAsync(Guid id)
+		{
+			var deleted = await _service.SoftDeleteAsync(id);
+			return Ok(deleted);
+		}
+	}
+}
+
+
+
