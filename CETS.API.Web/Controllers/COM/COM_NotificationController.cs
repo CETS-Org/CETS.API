fd77a05ddcb07e8681840274eebc308b0fb0aa9b
Minhaza <minhazaaa1006@gmail.com>
2025-09-03 15:00:58 +0700

    feat: add COM apis

---
+++ CETS.API.Web/Controllers/COM/COM_NotificationController.cs
@@ -0,0 +1,57 @@
+using Application.Interfaces.COM;
+using DTOs.COM_Notification.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CETS.API.Web.Controllers.COM
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class COM_NotificationController : ControllerBase
+	{
+		private readonly ICOM_NotificationService _service;
+
+		public COM_NotificationController(ICOM_NotificationService service)
+		{
+			_service = service;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllAsync()
+		{
+			var items = await _service.GetAllAsync();
+			return Ok(items);
+		}
+
+		[HttpGet("{id:guid}")]
+		public async Task<IActionResult> GetByIdAsync(Guid id)
+		{
+			var item = await _service.GetByIdAsync(id);
+			if (item == null) return NotFound();
+			return Ok(item);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateAsync([FromBody] CreateNotificationRequest request)
+		{
+			var created = await _service.CreateAsync(request);
+			return Created("Created", created);
+		}
+
+		[HttpPut("{id:guid}")]
+		public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] UpdateNotificationRequest request)
+		{
+			var updated = await _service.UpdateAsync(id, request);
+			return Ok(updated);
+		}
+
+		[HttpDelete("{id:guid}")]
+		public async Task<IActionResult> DeleteAsync(Guid id)
+		{
+			await _service.DeleteAsync(id);
+			return NoContent();
+		}
+	}
+}
+
+
+
