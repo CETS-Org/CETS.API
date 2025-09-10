ac511f4bbb608f5b67bc3a5275a45e6e2f9d3b81
Minhaza <minhazaaa1006@gmail.com>
2025-09-10 13:50:41 +0700

    feat: add odata for account and course

--- CETS.API.Web/Controllers/ACAD/ACAD_CourseController.cs
+++ CETS.API.Web/Controllers/ACAD/ACAD_CourseController.cs
@@ -1,12 +1,16 @@
 ﻿using Application.Interfaces.ACAD;
+using DTOs.ACAD.ACAD_Course.Responses;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
 
 namespace CETS.API.Web.Controllers.ACAD
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ACAD_CourseController : ControllerBase
+    public class ACAD_CourseController : ODataController
     {
         private readonly ILogger<ACAD_CourseController> _logger;
         private readonly IACAD_CourseService _courseService;
@@ -16,12 +20,14 @@ namespace CETS.API.Web.Controllers.ACAD
             _logger = logger;
             _courseService = courseService;
         }
-
+        [EnableQuery]
         [HttpGet]
         public async Task<IActionResult> GetAllAsync()
         {
             var courses = await _courseService.GetAllCoursesForListAsync();
             return Ok(courses);
         }
+
+      
     }
 }
