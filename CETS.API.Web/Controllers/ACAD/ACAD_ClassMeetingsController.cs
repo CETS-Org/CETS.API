f1be8d01c1ace480fd7b67f5fb3b28b289b21c7b
duongphan <duong.pnt@savvycom.vn>
2025-09-27 14:07:42 +0700

    feat : view student learning schedule

---
+++ CETS.API.Web/Controllers/ACAD/ACAD_ClassMeetingsController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces.ACAD;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace CETS.API.Web.Controllers.ACAD
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ACAD_ClassMeetingsController : ControllerBase
+    {
+        private readonly IACAD_ClassMeetingsService _classMeetingsService;
+
+        public ACAD_ClassMeetingsController(IACAD_ClassMeetingsService classMeetingsService)
+        {
+            _classMeetingsService = classMeetingsService;
+        }
+
+        [EnableQuery]
+        [HttpGet("Schedule/{studentId}")]
+        public async Task<IActionResult> GetByStudentAsync(Guid studentId, CancellationToken ct)
+        {
+            var schedules = await _classMeetingsService.WeeklyScheduleGetByStudentAsync(studentId, ct);
+            return Ok(schedules);
+        }
+
+    }
+}
