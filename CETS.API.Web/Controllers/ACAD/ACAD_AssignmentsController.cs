35f0926dc6ac94f56711c69ff026abb2c993cdc6
Huynh Han <ngochan9203@gmail.com>
2025-09-30 01:08:54 +0700

    feat : add UC View Assugment and Submission

---
+++ CETS.API.Web/Controllers/ACAD/ACAD_AssignmentsController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Implementations.ACAD;
+using Application.Interfaces.ACAD;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CETS.API.Web.Controllers.ACAD
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ACAD_AssignmentsController : ControllerBase
+    {
+        private readonly IACAD_AssignmentService _AssignmentService;
+
+        public ACAD_AssignmentsController(IACAD_AssignmentService AssignmentService)
+        {
+            _AssignmentService = AssignmentService;
+        }
+
+        [HttpGet("class-meeting/{classMeetingId}/student/{studentId}/assignments")]
+        public async Task<IActionResult> GetAssignmentsAndSubmissions(Guid classMeetingId, Guid studentId)
+        {
+            var result = await _AssignmentService.GetAssignmentsAndSubmissions(classMeetingId, studentId);
+            return Ok(result);
+        }
+
+
+    }
+}
